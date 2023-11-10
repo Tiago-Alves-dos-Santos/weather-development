9c9d600f72d690db5872fae0adbfd2b4590b67c1
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-11-10 10:00:01 -0300

    element plus instaled

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
+/** ElementPlus */
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+/** App and router */
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+.use(ElementPlus)
+.use(router)
+.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  lintOnSave: false,
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'FastWeather'; // Altere 'Novo Título' para o título desejado
+      return args;
+    });
+  },
+  //process.env.NODE_ENV === 'production' ?'/VueCli/':'/'
+  publicPath: '/weather-development/',
   transpileDependencies: true
 })
