fd1eb7428063a048d9cbcc8388497fd179e15736
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-11-16 18:55:37 -0300

    rename url

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
     });
   },
   //process.env.NODE_ENV === 'production' ?'/VueCli/':'/'
-  publicPath: '/weather-development/',
+  publicPath: '/weather/',
   transpileDependencies: true,
 
   devServer: {
